5ef1d5311bddd5b9f9258b5704b1df6ab0fbd24c
karthick587 <85030277+karthick587@users.noreply.github.com>
2021-11-13 00:48:52 +0530

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,5 +1,5 @@
 <?php
-if($_POST["Message"]) {
+if($_POST["name","email","message"]) {
 mail("john@johndoe.com", "Here is the sample subject line",
 $_POST["Insert Your Message"]. "From: jane@janedoe.com");
 }
